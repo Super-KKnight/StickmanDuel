525d9861c8a846c6d82b30a29ec6079f06c2d730
kk <1019625941@qq.com>
2025-04-27 22:20:15 +0800

    feature-operation1

--- src/main/java/com/Master/operationA.java
+++ src/main/java/com/Master/operationA.java
@@ -3,6 +3,6 @@ package com.Master;
 public class operationA {
     public operationA() {
         System.out.println("master-operationA");
-        System.out.println("feature-operationA");
+        System.out.println("feature-operation1");
     }
 }
