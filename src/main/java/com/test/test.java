f8a61e933af0d98cce0991a1862a8d44fbcdf7e2
kk <1019625941@qq.com>
2025-04-27 20:10:16 +0800

    更新installGame()

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -8,10 +8,10 @@ public class test {
         example.sayHello();
         downVersion();
         updateVersion();
-        gameUninstalls();
+        installGame();
     }
     public static void installGame(){
-        System.out.println("installGame！");
+        System.out.println("下载火柴人激斗！");
     }
     public static void gameName() {
         System.out.println("火柴人激斗");
@@ -21,8 +21,5 @@ public class test {
     }
     public static void downVersion(){
         System.out.println("版本降级");
-    }
-    public static void gameUninstalls(){
-        System.out.println("应用已卸载");
     }
 }
