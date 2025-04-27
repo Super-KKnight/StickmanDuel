597f65e7b3b9b7c6671446d21302b54893dadca7
墨染 <2513181230@qq.com>
2025-04-27 19:37:58 +0800

    这是测试

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -5,10 +5,9 @@ import com.example.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("拉取远程仓库测试");
-        System.out.println("wjy是超级大天才");
         example.sayHello();
     }
-    public static void gamename() {
+    public static void gameName() {
         System.out.println("火柴人大乱斗");
     }
 }
