debb0bd9b6d346e58bd580a4e49101b28f3c49b1
墨染 <2513181230@qq.com>
2025-04-27 18:28:03 +0800

    这是测试

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -5,7 +5,7 @@ import com.example.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("拉取远程仓库测试");
-        System.out.println("wjy提交的代码块测试");
+        System.out.println("wjy是天才");
         example.sayHello();
     }
 }
