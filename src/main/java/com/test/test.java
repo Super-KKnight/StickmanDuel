7d0a67a7f70a6a33281ce090755b1848d2113bf8
kk <1019625941@qq.com>
2025-04-28 16:04:05 +0800

    添加javaFx

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -1,30 +1,7 @@
 package com.test;
 
-
-
 public class test {
-    private final static double version = 0.1;
-
     public static void main(String[] args) {
-        installGame();
-        gameName();
-        updateVersion();
-        downVersion();
-    }
-
-    public static void installGame() {
-        System.out.println("下载火柴人激斗！"+version);
-    }
-
-    public static void gameName() {
-        System.out.println("火柴人激斗");
-    }
-
-    public static void updateVersion() {
-        System.out.println("版本更新");
-    }
-
-    public static void downVersion() {
-        System.out.println("版本降级");
+        System.out.println("Hello World");
     }
 }
